80013cfcb15bd0d98fdcfecbb6b97b7b01db4ac8
Chuck <chuck@eclecticsaddlebag.com>
2023-06-21 10:06:20 -0400

    Remove mailgun

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,32 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
-  // add your custom rules here
-  rules: {
-    "vue/html-closing-bracket-newline": ["error", {
-      "singleline": "never",
-      "multiline": "never"
-    }],
-    "vue/max-attributes-per-line": ["error", {
-      "singleline": 3,
-      "multiline": {
-        "max": 1,
-        "allowFirstLine": true
-      }
-    }],
-    "vue/html-indent": ["error", 2, {
-      "alignAttributesVertically": false,
-    }],
-    "template-curly-spacing" : "off",
-    indent: "off",
-    'semi': 0,
-    'baseIndent': 0
-  }
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    extends: [
+        '@nuxtjs',
+        'plugin:nuxt/recommended'
+    ],
+    // add your custom rules here
+    rules: {
+        "vue/html-closing-bracket-newline": ["error", {
+            "singleline": "never",
+            "multiline": "never"
+        }],
+        "vue/max-attributes-per-line": ["error", {
+            "singleline": {"max": 3},
+            "multiline": {"max": 1,}
+        }],
+        "vue/html-indent": ["error", 2, {
+            "alignAttributesVertically": false,
+        }],
+        "template-curly-spacing": "off",
+        indent: "off",
+        'semi': 0,
+        'baseIndent': 0
+    }
 }
